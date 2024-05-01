bac2fcc22df1105f1c4f55b786355b068187f8c2
bhavani1226 <168573834+bhavani1226@users.noreply.github.com>
2024-05-01 11:18:22 +0530

    Add files via upload

---
+++ Normal.java
@@ -0,0 +1,17 @@
+package com.si.indu;
+
+public class Normal {
+	
+    void display1() {
+		
+		System.out.println("it is a instance method");
+		
+	}
+	
+	static void display2() {
+		
+		System.out.println("it is a static method");
+				
+	}
+
+}
