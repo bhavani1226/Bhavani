1004ee1c26c65aaece39f5190978d0e62663942a
bhavani1226 <168573834+bhavani1226@users.noreply.github.com>
2024-05-01 11:09:51 +0530

    Add files via upload

---
+++ Instancestatic.java
@@ -0,0 +1,23 @@
+package com.s3.indu;
+
+public class Instancestatic {
+	void display()
+	{
+		System.out.println(" this is parent class");
+	}
+    void display1 (int a)
+    {
+    	System.out.println(a);
+    	
+    }
+     static void show()
+     {
+    	 System.out.println("this is static method");
+    	 
+     }
+     static void show (int b)
+     {
+    	 System.out.println(b);
+    	
+     }
+}
---
+++ Mainclass.java
@@ -0,0 +1,14 @@
+package com.s3.indu;
+
+public class Mainclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ Instancestatic t=new Instancestatic();
+ t.display();
+ t.display1(2);
+ t.show();
+ t.show(6);
+	}
+
+}
