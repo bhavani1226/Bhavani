8b9b40e70a444628e5df0998d8b10bb70635c3ce
bhavani1226 <168573834+bhavani1226@users.noreply.github.com>
2024-05-01 10:02:00 +0530

    Add files via upload

---
+++ Implement.java
@@ -0,0 +1,17 @@
+package com.si.indu;
+
+public class Implement {
+
+	public static void main(String[] args) {
+		
+		Normal obj=new Normal();
+		
+		obj.display1();
+		
+		Normal.display2();
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
