37fee6a8250199b2f0c0723022dffa015c61d639
bhavani1226 <168573834+bhavani1226@users.noreply.github.com>
2024-05-07 22:21:50 +0530

    Add files via upload

---
+++ Cse.java
@@ -0,0 +1,8 @@
+package branch;
+
+public class Cse {
+	public void msg() {
+	System.out.println("i am from cse");
+}
+
+}
---
+++ Ece.java
@@ -0,0 +1,8 @@
+package branch;
+
+public class Ece {
+	public void msg() {
+		System.out.println("i am from ece");
+	}
+
+}
