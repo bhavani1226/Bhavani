92336f39bee21f16db606b96c6a88e8721c4048b
bhavani1226 <168573834+bhavani1226@users.noreply.github.com>
2024-05-07 22:23:23 +0530

    Add files via upload

---
+++ It.java
@@ -0,0 +1,21 @@
+package course;
+
+import branch.Cse;
+import branch.Ece;
+import subbranch.Aiml;
+
+public class It {
+
+	public static void main(String[] args) {
+		
+		Cse obj=new Cse();
+		Ece obj1=new Ece();
+		Aiml obj2=new Aiml();
+		obj.msg();
+		obj1.msg();
+		obj2.msg();
+		// TODO Auto-generated method stub
+
+	}
+
+}
